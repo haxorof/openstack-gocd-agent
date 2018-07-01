a4b18ccc81319c3a0ac3ecf2d7b964157ab8f27a
Björn Oscarsson <haxorof@users.noreply.github.com>
2018-07-01 16:55:40 +0200

    Added Vagrant image based on artful.json

---
+++ ubuntu/scripts/install-vagrant.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo '--> Installing Vagrant'
+sudo apt-get -y install vagrant
---
+++ ubuntu/scripts/install-virtualbox.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+echo '--> Installing VirtualBox'
+echo "deb https://download.virtualbox.org/virtualbox/debian artful contrib" | sudo tee /etc/apt/sources.list.d/gocd.list
+sudo apt-key adv --fetch-keys https://www.virtualbox.org/download/oracle_vbox_2016.asc
+sudo apt-key adv --fetch-keys https://www.virtualbox.org/download/oracle_vbox.asc
+sudo apt-get update
+sudo apt-get -y install virtualbox
