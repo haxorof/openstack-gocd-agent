537e5073f0bb4e34ddfb6ebb2b2fa088bbdb5066
Björn Oscarsson <haxorof@users.noreply.github.com>
2018-12-22 21:39:40 +0100

    Added autoremove after upgrade

--- ubuntu/scripts/upgrade-pkgs.sh
+++ ubuntu/scripts/upgrade-pkgs.sh
@@ -2,3 +2,4 @@
 echo '--> Upgrading packages'
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+sudo apt-get -y autoremove
