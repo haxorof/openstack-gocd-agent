cb0f1332ff76bde479a25fdafe6d2aaade7ce72e
Björn Oscarsson <haxorof@users.noreply.github.com>
2018-12-22 20:43:44 +0100

    Updated versions

--- ubuntu/scripts/install-vagrant.sh
+++ ubuntu/scripts/install-vagrant.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo '--> Installing Vagrant'
-wget -O vagrant.deb https://releases.hashicorp.com/vagrant/2.2.0/vagrant_2.2.0_x86_64.deb
+wget -O vagrant.deb https://releases.hashicorp.com/vagrant/2.2.2/vagrant_2.2.2_x86_64.deb
 sudo dpkg -i vagrant.deb
 rm vagrant.deb
