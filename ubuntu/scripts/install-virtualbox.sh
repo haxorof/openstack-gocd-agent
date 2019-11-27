22ab93d990551183780b8f2fbd8dac71fd006919
Bjorn Oscarsson <haxorof@users.noreply.github.com>
2019-11-27 07:23:43 +0100

    Updated virtualbox

--- ubuntu/scripts/install-virtualbox.sh
+++ ubuntu/scripts/install-virtualbox.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo '--> Installing VirtualBox'
-echo "deb https://download.virtualbox.org/virtualbox/debian artful contrib" | sudo tee /etc/apt/sources.list.d/gocd.list
+sudo sh -c 'echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" >> /etc/apt/sources.list.d/virtualbox.list'
 sudo apt-key adv --fetch-keys https://www.virtualbox.org/download/oracle_vbox_2016.asc
 sudo apt-key adv --fetch-keys https://www.virtualbox.org/download/oracle_vbox.asc
 sudo apt-get update
-sudo apt-get -y install virtualbox-5.2
+sudo apt-get -y install virtualbox-6.0
