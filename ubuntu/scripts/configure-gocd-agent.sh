be8ddfa7e739e43f42eda399f0b84df92fe3e1af
Bjorn Oscarsson <haxorof@users.noreply.github.com>
2019-11-27 22:02:04 +0100

    Fix go-agent configuration

--- ubuntu/scripts/configure-gocd-agent.sh
+++ ubuntu/scripts/configure-gocd-agent.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 echo '--> Add GoCD agent to cloud-init'
+# sudo wget -O /usr/lib/python3/dist-packages/cloudinit/config/cc_openstack_gocd.py \
+#   https://raw.githubusercontent.com/gocd-contrib/openstack-elastic-agent-tools/450d8c1469728bb40669ff658b91b4ddab3d6b49/cloudinit/cc_openstack_gocd.py
 sudo wget -O /usr/lib/python3/dist-packages/cloudinit/config/cc_openstack_gocd.py \
-  https://raw.githubusercontent.com/gocd-contrib/openstack-elastic-agent-tools/450d8c1469728bb40669ff658b91b4ddab3d6b49/cloudinit/cc_openstack_gocd.py
+  https://raw.githubusercontent.com/haxorof/openstack-elastic-agent-tools/master/cloudinit/cc_openstack_gocd.py
 sed -e '/cloud_final_modules:/a\' -e ' - openstack_gocd' /etc/cloud/cloud.cfg | sudo tee /etc/cloud/cloud.cfg
 
 echo '--> Allowing go user full sudo rights'
